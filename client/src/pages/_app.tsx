fbe18344770dfe1466a5b84594676f66d0554ffc
Sayan Mallick <snyxmk@gmail.com>
2022-04-17 20:51:32 +0530

    moving to websockets

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import { ChakraProvider } from '@chakra-ui/react'
 
 import theme from '../theme'
+import { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
       <Component {...pageProps} />
