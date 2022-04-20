e4a3d8fb3971b77080fc8dd8e0e777e19660772e
Sayan Mallick <snyxmk@gmail.com>
2022-04-20 20:56:45 +0530

    Final build for the client and server

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,28 +1,84 @@
-import { useCallback } from "react";
+import {
+  Box,
+  Container,
+  Stack,
+  Text,
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button,
+  InputLeftAddon
+} from '@chakra-ui/react';
+import { useState, useRef, useEffect } from "react";
 import useWebSocket, { ReadyState } from "react-use-websocket";
 
-const Index = () => {
-  const { readyState, sendJsonMessage, lastJsonMessage } = useWebSocket("ws://localhost:4000");
 
-  const handleClick = useCallback(() => {
-    sendJsonMessage({
-      message: "Hello from the client!"
-    });
+const Index = () => {
+  const [dutyCycle, setDutyCycle] = useState(0);
+  const [frequency, setFrequency] = useState(0);
+
+  let frequencyInput = useRef<HTMLInputElement>(null);
+
+  const { readyState, sendJsonMessage } = useWebSocket("ws://localhost:4000");
+
+  const handleFrequency = () => {
+    const temp = frequencyInput.current
+    setFrequency(parseInt(temp.value));
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:8000/")
+      .then(res => res.json())
+      .then(data => {
+        setFrequency(data.frequency);
+        setDutyCycle(data.dutyCycle);
+      }
+      );
   }, []);
 
-  if (lastJsonMessage !== null) {
-    console.log(lastJsonMessage);
-  }
+  useEffect(() => {
+    if (readyState === ReadyState.OPEN) {
+      sendJsonMessage({
+        frequency: frequency,
+        dutyCycle: dutyCycle
+      });
+    }
+  }, [dutyCycle, frequency]);
+
 
   return (
-    <button
-      onClick={handleClick}
-      disabled={readyState !== ReadyState.OPEN}
-    >
-      Click Me To Send "Hello"
-    </button>
-  )
+    <>
+      <Container maxW={'3xl'} >
+        <Stack
+          as={Box}
+          textAlign={'center'}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}
+        >
+          <Text fontSize="4xl">Motor Duty Cycle : {dutyCycle}% </Text>
+          <InputGroup>
+            <InputLeftAddon>Frequency</InputLeftAddon>
+            <Input defaultValue={frequency} ref={frequencyInput} />
+            <InputRightElement width={'4rem'}>
+              <Button h='1.75rem' size='sm' bg="tomato" onClick={handleFrequency}>Set</Button>
+            </InputRightElement>
+          </InputGroup>
+          <Slider value={dutyCycle} min={0} max={100} step={1} onChange={(val) => setDutyCycle(val)}>
+            <SliderTrack bg='red.100'>
+              <Box position='relative' right={10} />
+              <SliderFilledTrack bg='tomato' />
+            </SliderTrack>
+            <SliderThumb boxSize={6} />
+          </Slider>
 
+        </Stack>
+      </Container>
+    </>
+  )
 }
 
 export default Index
--- server/server.ts
+++ server/server.ts
@@ -3,20 +3,29 @@ import { createServer, ServerResponse, RequestOptions } from "http";
 
 const ws = new WebSocketServer({ port: 4000 });
 
-let state = "";
+type motorData = {
+  frequency: number;
+  dutyCycle: number;
+}
+
+let state: motorData = {
+  frequency: 400,
+  dutyCycle: 0
+};
 
 ws.on("connection", (ws) => {
   ws.on("message", (message) => {
-    state = message.toString();
-    console.log(message.toString());
+    console.log(JSON.parse(message.toString()));
+    state = JSON.parse(message.toString());
     ws.send(message.toString());
   });
 });
 
 const requestHandler = (_request: RequestOptions, response: ServerResponse) => {
+  response.setHeader('Access-Control-Allow-Origin', '*');
   response.setHeader("Content-Type", "application/json");
   response.writeHead(200);
-  response.end(`{"state": "${state}"}`);
+  response.end(JSON.stringify(state));
 }
 
 const http = createServer(requestHandler);
