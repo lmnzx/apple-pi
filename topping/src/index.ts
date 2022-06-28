c60b4821f6f87ed7f3734c7ceca621b4d4050b11
Sayan Mallick <snyxmk@gmail.com>
2022-06-28 20:39:32 +0530

    passwordless auth setup

--- topping/src/index.ts
+++ topping/src/index.ts
@@ -1,5 +1,26 @@
 import fastify from "fastify";
 import cors from "@fastify/cors";
+import { MongoClient } from "mongodb";
+import Redis from "ioredis";
+import { randomUUID } from "crypto";
+import nodemailer, { SendMailOptions } from "nodemailer";
+
+require("dotenv").config();
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri!);
+
+const redis = new Redis(process.env.REDIS_URI!);
+
+let transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  auth: {
+    user: "jayda.becker90@ethereal.email",
+    pass: "HRKDkk3A6qjvCr7WnB",
+  },
+} as SendMailOptions);
 
 const server = fastify({
   logger: {
@@ -15,26 +36,51 @@ const server = fastify({
   },
 });
 
-type FormValues = {
-  email: string;
-};
+(async () => {
+  await client.connect();
 
-server.register(cors, {
-  origin: "*",
-});
+  const database = client.db("apple-pi");
+  const users = database.collection("users");
 
-server.get("/", async (request, reply) => {
-  return "pong\n";
-});
+  type FormValues = {
+    email: string;
+  };
 
-server.post<{ Body: FormValues }>("/signin", async (request, reply) => {
-  server.log.info(request.body.email);
-});
+  server.register(cors, {
+    origin: "*",
+  });
 
-server.listen({ port: 8000 }, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`server listening on ${address}`);
-});
+  server.get("/", async (request, reply) => {
+    const allUsers = await users.find({}).toArray();
+    return allUsers;
+  });
+
+  server.post<{ Body: FormValues }>("/signin", async (request, reply) => {
+    server.log.info(request.body.email);
+    const user = await users.findOne({ email: request.body.email });
+    if (!user) {
+      await users.insertOne({
+        email: request.body.email,
+        createdAt: new Date(),
+      });
+      server.log.info("User created");
+    }
+    const uuid = randomUUID();
+    redis.set(request.body.email, uuid, "EX", 60 * 60 * 2); // 2 hours expiration
+    const info = await transporter.sendMail({
+      from: '"Lemon 🍋" <noreply@apple.pi>',
+      to: request.body.email,
+      subject: "Login Url",
+      text: `http://localhost:3000/auth/${uuid}`,
+    });
+    server.log.info("Message sent: %s", info.messageId);
+  });
+
+  await server.listen({ port: 8000 }, (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`server listening on ${address}`);
+  });
+})();
